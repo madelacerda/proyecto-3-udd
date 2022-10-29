a2f31886bd807297edab8734b48eaf9e98fc00a5
madelacerda <53839267+madelacerda@users.noreply.github.com>
2022-10-29 20:32:50 -0300

    mostrar precio del dolar

--- proyecto-3/src/components/Dolar.js
+++ proyecto-3/src/components/Dolar.js
@@ -1,21 +1,53 @@
 import axios from "axios";
-import React from "react";
+import React, { useState } from "react";
+import { LineChart } from "./Grafico";
+import { UserData } from "./Data";
 
 const getAPI = async (url) => {
   const res = await axios.get(url);
-  return res.data.serie;
+  return res.data;
 };
 
-let valoruf = 0;
-getAPI("https://mindicador.cl/api/uf/22-10-2022").then((data) => {
-  valoruf = data[0].valor;
+let tasasArray = [];
+let fechasArray = [];
+let nombre = "";
+
+getAPI("https://mindicador.cl/api/dolar/").then((data) => {
+  nombre = data.nombre;
+
+  let tasas = data.serie;
+  console.log(tasas);
+  tasasArray = tasas.map((serie) => serie.valor);
+  fechasArray = tasas.map((serie) => serie.fecha.split("T")[0]);
+  console.log(tasasArray);
+  console.log(fechasArray);
+  console.log(nombre);
 });
 
 function Dolar() {
+  const [userData, setUserData] = useState({
+    labels: fechasArray,
+    datasets: [
+      {
+        label: nombre,
+        data: tasasArray,
+        backgroundColor: [
+          "rgba(75,192,192,1)",
+          "#ecf0f1",
+          "#50AF95",
+          "#f3ba2f",
+          "#2a71d0",
+        ],
+        borderColor: "black",
+        borderWidth: 2,
+      },
+    ],
+  });
+
   return (
     <div>
-      <h2>Valor Dolar</h2>
-      <p>El valor de la uf de hoy es: {valoruf}</p>
+      {/* <Grafico chartData={tasasArray} nombre={nombre} fechasArray={fechasArray} /> */}
+      <LineChart chartData={userData} />
     </div>
   );
 }
